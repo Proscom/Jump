95a61305691d36e19fd7b588a170d0f80955b9f9
MAXIM KOLESNIK <maxim@sugarandcandy.ru>
2017-01-08 04:36:56 +0300

    fix

---
+++ Jump/Jump.h
@@ -0,0 +1,19 @@
+//
+//  Jump.h
+//  Jump
+//
+//  Created by MAXIM KOLESNIK on 08.01.17.
+//  Copyright © 2017 Proscom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Jump.
+FOUNDATION_EXPORT double JumpVersionNumber;
+
+//! Project version string for Jump.
+FOUNDATION_EXPORT const unsigned char JumpVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Jump/PublicHeader.h>
+
+
